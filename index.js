31ad04cecb221b1d42b1414220a68d5192091e29
oldhamste <50807817+oldhamste@users.noreply.github.com>
2025-11-18 07:32:52 +0000

    Update index.js
    update

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("TikTok Connector Running"));
+app.listen(process.env.PORT || 3000);
 // Main server file
 console.log('TikTok Connector Loaded');
